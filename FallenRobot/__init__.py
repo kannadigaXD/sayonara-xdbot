f14220d12b77c9e198085f6371f40694214827f0
Kannadiga XD <128951142+kannadigaXD@users.noreply.github.com>
2023-05-08 10:36:59 +0530

    Update __init__.py

--- FallenRobot/__init__.py
+++ FallenRobot/__init__.py
@@ -45,10 +45,10 @@ if ENV:
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     NO_LOAD = os.environ.get("NO_LOAD", "").split()
     START_IMG = os.environ.get(
-        "START_IMG", "https://telegra.ph/file/8c78951f44aa036252af9.jpg"
+        "START_IMG", "https://te.legra.ph/file/8ce12e56b9ef1ca8a575c.jpg"
     )
     STRICT_GBAN = bool(os.environ.get("STRICT_GBAN", True))
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "kimjikoinsupport")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "sayonaraxdbot")
     TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY", "./")
     TOKEN = os.environ.get("TOKEN", None)
     TIME_API_KEY = os.environ.get("TIME_API_KEY", None)
@@ -142,7 +142,7 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1356469075)
+DEV_USERS.add(6181817811)
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
